d9dc04e727c6adc7efaab634b2905a7f5633911b
Prosen Biswas <webprosen@gmail.com>
2023-03-25 23:59:45 +0600

    first commit

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Cosmetics = () => {
+
+    const cosmetics = [
+        {id: 1, name: 'Alta', price: 100},
+        {id: 2, name: 'Balta', price: 200},
+        {id: 3, name: 'Calta', price: 300},
+        {id: 4, name: 'Dalta', price: 400},
+        {id: 5, name: 'Ealta', price: 500}
+    ];
+
+    return (
+        <div>
+            <h1>Welcome to my Cosmetics Store!!!</h1>
+        </div>
+    );
+};
+
+export default Cosmetics;
